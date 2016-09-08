d34a17671a1b5f3caac3a2887404e2b7c8646f25
Aaron <aaron.devleague@gmail.com>
2016-09-07 20:13:02 -1000

    secret number generator

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,11 @@
 'use strict';
 module.exports = function() {
-  
+
+ var sng = Math.floor((Math.random()*1000000)+1);
+ function secretNumber(){
+    return sng;
+  }
+
+  return secretNumber;
+
 };
